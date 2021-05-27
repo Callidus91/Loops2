613c08e3cab7e7aebbb8ce35c050cc0683313a3f
trgarner <trgarner91@gmail.com>
2021-05-27 11:59:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowHeight = 4;
+            for (int row = 1; row <= rowHeight; row++)
+            {
+                
+                for (int j = 0; j < row; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
